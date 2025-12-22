13588c2e4070010410bff4a9631081c6178d7a7c
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-12-22 18:37:55 +0300

    добавил правило, что константы должны быть в UPPER_CASE

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,19 +6,32 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "htmlacademy/react-typescript",
+    'htmlacademy/react-typescript',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: 'tsconfig.json' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
   settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        modifiers: ['const', 'global'],
+        types: ['string', 'number', 'boolean'],
+        format: ['UPPER_CASE'],
+      },
+    ],
   },
   overrides: [
     {
-      files: [ '*test*' ],
-      rules: { '@typescript-eslint/unbound-method': 'off' }
+      files: ['*test*'],
+      rules: { '@typescript-eslint/unbound-method': 'off' },
     },
   ],
-}
+};
